d0ea1534779e863b0379ff50590bdcb43214ce25
Joshua Chan <joshua@Joshuas-MBP.home>
2016-01-12 17:40:22 -0600

    initial commit

---
+++ main/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    # return HttpResponse("Hello, World!")
+    return render(request, "main/home.html", { 'message': 'hi there!'})
